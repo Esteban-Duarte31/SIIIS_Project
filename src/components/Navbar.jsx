40a704049b3685bac5e7a3e80b1452f69c1e530b
Dev-JimmyZea <jimmy26.dev@gmail.com>
2022-05-16 11:39:41 -0500

    style(Root): add z-index to dropdown menu profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -96,7 +96,7 @@ const Navbar = () => {
                                             <BellIcon className="h-6 w-6" aria-hidden="true" />
                                         </button>
                                         {/* Profile dropdown */}
-                                        <Menu as="div" className="ml-3 relative">
+                                        <Menu as="div" className="ml-3 z-20">
                                             <div>
                                                 <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                                                     <span className="sr-only">Open user menu</span>
